af6014547e664794daf3a1f6dcf27c258e81d66d
CheongLoic <huangloic@hotmail.com>
2023-06-18 05:39:44 +0200

    test merge

--- client/src/database/check_barcode.js
+++ client/src/database/check_barcode.js
@@ -1,23 +1,18 @@
-// import products from './products.json'
-// const product = require('products.json')
-
 const fs = require("fs");
 const productFilename = "products.json"
-  const loadJSON_products = JSON.parse(fs.existsSync(productFilename)) ? fs.readFileSync(productFilename).toString()  : '""' 
-  const products = JSON.parse(loadJSON_products); //string to JSON object 
+const loadJSON_products = JSON.parse(fs.existsSync(productFilename)) ? fs.readFileSync(productFilename).toString()  : '""' 
+const products = JSON.parse(loadJSON_products); //string to JSON object 
 
-const test =(product) =>{
+const test =() =>{
   let count_wrong_barcode =0
     products.map((product) => {
         product.barCode_list.map((BARCODELIST) => {
             if (BARCODELIST.barCode.length !== 13  && product.typeOfSale === "unit" && product.barCode_available === true ) {
               count_wrong_barcode +=1
               return (console.log(product.product_id, " ", product.product_full_name, " ", BARCODELIST.barCode))}
-    
         })
     })
-    console.log('count_wrong_barcode', count_wrong_barcode)
-
+    console.log('count_wrong_barcode : ', count_wrong_barcode)
 }
 
 test()
